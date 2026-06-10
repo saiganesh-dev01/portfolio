a44074f166637fa639dd45d7d31ffc8ac687c380
Sai Ganesh <saiganesh.dev01@gmail.com>
2026-06-10 12:14:14 +0530

    Added responsive navbar

--- src/componenet/NavBar.jsx
+++ src/componenet/NavBar.jsx
@@ -1,27 +1,47 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 import "./navbar.css"
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false)
+
     const navItems = [
         { name: "About", path: "/about" },
         { name: "Skills", path: "/skills" },
         { name: "Projects", path: "/projects" },
-        {name:"Contact", path:"/contact"}
+        { name: "Contact", path: "/contact" }
     ]
 
     return (
         <div className="navbar">
-            <Link to="/" className="navitem"><h3>Maram Sai Ganesh Reddy</h3></Link>
+            <Link to="/" className="navitem"><h3>Sai Ganesh</h3></Link>
+
             <div id="navitems">
                 {navItems.map((item) => (
-                    <div>
-                        <Link key={item.path} to={item.path}  className="navitem">
+                    <div key={item.path}>
+                        <Link to={item.path} className="navitem">
                             <h3>{item.name}</h3>
                         </Link>
                     </div>
                 ))}
             </div>
+
+            <div id="navbtn" onClick={() => setIsOpen(!isOpen)}>
+                <h3>Menu</h3>
+            </div>
+
+            {isOpen && (
+                <div id="resnavitems">
+                    {navItems.map((item) => (
+                        <div key={item.path}>
+                            <Link to={item.path} className="resnavitem">
+                                <h3>{item.name}</h3>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
