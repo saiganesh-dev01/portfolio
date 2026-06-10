d4b50e321d5e9f5965211ce9c5ff0653c2fd0f74
Sai Ganesh <saiganesh.dev01@gmail.com>
2026-06-10 12:34:09 +0530

    Added socials to home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,10 +3,15 @@ import "./home.css"
 export default function Home() {
     return (
         <div id="home">
-            <h2>Hi 👋 there!</h2>
             {/* <p>📍BASED IN VIZAG, INDIA</p> */}
             <h1>Maram Sai Ganesh Reddy</h1>
             <p>CS Student interested in Robotics and Web Development</p>
+            <div id="social-links">
+                <a href="https://github.com/saiganesh-dev01" target="_blank">Github</a>|
+                <a href="https://github.com/saiganesh-dev01" target="_blank">LinkedIn</a>|
+                <a href="https://github.com/saiganesh-dev01" target="_blank">Twitter</a>|
+                <a href="https://github.com/saiganesh-dev01" target="_blank">Mail</a>
+            </div>
         </div>
     )
 }
